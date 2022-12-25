3471dc82754d77dc32aef7be7282938410571176
Hagve_rq27 <ayxanxakverdiyev1@gmail.com>
2022-12-25 17:38:00 +0400

    dsajd;k

---
+++ yyy1.py
@@ -0,0 +1,55 @@
+# Task 1
+# i = 0
+#
+# while i < 7:
+#     j = 0
+#
+#     while j < 9:
+#         if j >= 1 and j < 8 and i > 0 and i < 6:
+#             print("  ", end="")
+#             j += 1
+#             continue
+#
+#         print("* ", end="")
+#         j += 1
+#     print()
+#     i += 1
+
+# Task 2
+# i = 0
+#
+# while i < 3:
+#     j = 0
+#
+#     while j < 7:
+#         if j >= 1 and j < 6 and i > 0 and i < 2:
+#             print("  ", end="")
+#             j += 1
+#             continue
+#
+#         print("* ", end="")
+#         j += 1
+#     print()
+#     i += 1
+
+
+
+
+
+
+# Task 3
+# i = 0
+#
+# while i < 7:
+#     j = 0
+#
+#     while j < 3:
+#         if j >= 1 and j < 2 and i > 0 and i < 6:
+#             print("  ", end="")
+#             j += 1
+#             continue
+#
+#         print("* ", end="")
+#         j += 1
+#     print()
+#     i += 1
